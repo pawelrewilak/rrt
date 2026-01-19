d738f493ce9a23b88385cee522ca3b5670c50a74
SomeKindOfMagician <33197572+SitoMikolaj@users.noreply.github.com>
2026-01-19 17:39:00 +0100

    Skończono sprawozdanie, dodano nowy plik na stare algorytmy, uzupełniono komentarze

---
+++ old_rrt.py
@@ -0,0 +1,413 @@
+#Kod zawierający nieużywane programy funkcji
+
+# Warianty funkcji
+
+# def rrt_ellipse(start, goal, mapa, step_len = 40, max_iter = 1000, tolerance = 3 ,goal_bias = 0.1):
+
+
+    
+#     plt.ion()
+#     h, w = mapa.shape
+    
+#     fig, ax = plt.subplots(figsize=(12, 7)) 
+
+#     ax.set_xlim(0, w)
+#     ax.set_ylim(h, 0)
+#     ax.imshow(mapa, cmap='binary')
+#     ax.set_title("rrt_elipse", y=1.05, fontsize=14)
+    
+
+#     ax.scatter(start[0], start[1], c='red', s=100, marker='X', zorder=5)
+#     ax.scatter(goal[0], goal[1], c='green', s=100, marker='X', zorder=5)
+
+#     start_node = treeNode(start[0], start[1])
+#     tree = Tree(start_node)
+#     sr_x, sr_y, a, b, theta = get_ellipse_params(start, goal)
+#     ellipse_patch = patches.Ellipse((sr_x, sr_y), width=2*a, height=2*b, angle=math.degrees(theta), 
+#                                     fill=False, color='orange', linestyle='--', alpha=0.5, linewidth=1)
+#     ax.add_patch(ellipse_patch)
+#     iter_text = ax.text(0, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+
+
+#     plt.pause(1)
+
+#     for i in range(max_iter):
+
+#         iter_text.set_text(f'Iteracja: {i}')
+
+#         if random.random() < goal_bias:
+#             sample_node = treeNode(goal[0], goal[1])
+
+#         else:
+#             sample_node = samp_point_elipse(start,goal, mapa)
+
+#         pt, = ax.plot(sample_node.locationX, sample_node.locationY, marker='o', color='red', markersize=15)
+
+#         pot_parent = tree.nearest(sample_node)
+#         distance = tree.distance(pot_parent,sample_node)
+
+#         if distance <= step_len:
+#             new_node = sample_node
+#         else:
+
+#             stepX = ((sample_node.locationX - pot_parent.locationX) / distance) * step_len
+#             stepY = ((sample_node.locationY - pot_parent.locationY) / distance) * step_len
+#             new_node = treeNode(pot_parent.locationX + stepX, pot_parent.locationY + stepY)
+
+#         if collision_free(pot_parent,new_node,mapa):
+#             tree.add_node(new_node,pot_parent)
+#             ax.plot([pot_parent.locationX, new_node.locationX], [pot_parent.locationY, new_node.locationY], c='blue', linewidth=2)
+
+
+#             if tree.distance(new_node, treeNode(goal[0], goal[1])) < tolerance:
+#                 tree.add_node(treeNode(goal[0], goal[1]), new_node)
+#                 found_path = tree.path_recovery(tree.nodes[-1])
+#                 for node in found_path:
+#                     if node.parent is None:
+#                         pass
+#                     else:
+#                         ax.plot([node.locationX, node.parent.locationX], [node.locationY, node.parent.locationY],
+#                                 c='green', linewidth=4)
+#                         #plt.pause(0.03)
+
+#                 plt.ioff()
+#                 plt.show()
+
+#                 return f"Path found in {i} iterations", i
+
+
+#         #plt.pause(0.001)
+#         try:
+#             pt.remove()
+#         except:
+#             pass
+#     return "Path not found"
+
+# def rrt_ellipse_aiw(start, goal, mapa, step_len = 40, max_iter = 1000, tolerance = 3 , pmax = 0.4 , pmin = 0.05):
+
+    
+#     plt.ion()
+#     h, w = mapa.shape
+    
+#     fig, ax = plt.subplots(figsize=(12, 7)) 
+
+#     ax.set_xlim(0, w)
+#     ax.set_ylim(h, 0)
+#     ax.imshow(mapa, cmap='binary')
+#     ax.set_title("rrt_elipse_aiw", y=1.05, fontsize=14)
+    
+
+#     ax.scatter(start[0], start[1], c='red', s=100, marker='X', zorder=5)
+#     ax.scatter(goal[0], goal[1], c='green', s=100, marker='X', zorder=5)
+
+#     start_node = treeNode(start[0], start[1])
+#     tree = Tree(start_node)
+
+#     sr_x, sr_y, a, b, theta = get_ellipse_params(start, goal)
+#     ellipse_patch = patches.Ellipse((sr_x, sr_y), width=2*a, height=2*b, angle=math.degrees(theta), 
+#                                     fill=False, color='orange', linestyle='--', alpha=0.5, linewidth=1)
+#     ax.add_patch(ellipse_patch)
+
+#     iter_text = ax.text(0, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+#     weight_text = ax.text(0.2, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+#     succes_rate_text = ax.text(0.4, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+
+#     plt.pause(1)
+#     success = 0
+#     ps = 0.0
+#     pa = pmin
+    
+#     for i in range(1,max_iter + 1):
+
+#         ps = success / i
+
+#         pa = pmin + (pmax - pmin) * ps
+#         pa = np.clip(pa, pmin, pmax)
+
+#         if i % 5 == 0:
+#             iter_text.set_text(f'Iteracja: {i}') 
+#             weight_text.set_text(f'Goal Bias: {pa:.2f}')
+#             succes_rate_text.set_text(f'Success Rate: {ps:.3f}')
+
+
+#         if random.random() < pa:
+#             sample_node = treeNode(goal[0], goal[1])
+
+
+#         else:
+#             sample_node = samp_point_elipse(start,goal, mapa)
+
+#         pt, = ax.plot(sample_node.locationX, sample_node.locationY, marker='o', color='red', markersize=15)
+
+#         pot_parent = tree.nearest(sample_node)
+#         distance = tree.distance(pot_parent,sample_node)
+
+#         if distance <= step_len:
+#             new_node = sample_node
+            
+
+#         else:
+#             stepX = ((sample_node.locationX - pot_parent.locationX) / distance) * step_len
+#             stepY = ((sample_node.locationY - pot_parent.locationY) / distance) * step_len
+#             new_node = treeNode(pot_parent.locationX + stepX, pot_parent.locationY + stepY)
+
+#         if collision_free(pot_parent,new_node,mapa):
+#             tree.add_node(new_node,pot_parent)
+#             success += 1
+#             ax.plot([pot_parent.locationX, new_node.locationX], [pot_parent.locationY, new_node.locationY], c='blue', linewidth=2)
+
+
+#             if tree.distance(new_node, treeNode(goal[0], goal[1])) < tolerance:
+#                 tree.add_node(treeNode(goal[0], goal[1]), new_node)
+#                 found_path = tree.path_recovery(tree.nodes[-1])
+#                 for node in found_path:
+#                     if node.parent is None:
+#                         pass
+#                     else:
+#                         ax.plot([node.locationX, node.parent.locationX], [node.locationY, node.parent.locationY],
+#                                 c='green', linewidth=4)
+#                         #plt.pause(0.03)
+
+#                 plt.ioff()
+#                 plt.show()
+
+#                 return f"Path found! \nIterations: {i} \nFinal Goal Bias: {pa:.2f} \nSucces Rate: {100 * ps:.1f} % "
+
+#         #plt.pause(0.001)
+#         try:
+#             pt.remove()
+#         except:
+#             pass
+
+#     return "Path not found"
+
+# def rrt_ellipse_aiw_ldiw(start, goal, mapa, step_len = 40, max_iter = 1000, tolerance = 3 , pmax = 0.4 , pmin = 0.05):
+
+#     plt.ion()
+#     h, w = mapa.shape
+    
+#     fig, ax = plt.subplots(figsize=(12, 7)) 
+
+#     ax.set_xlim(0, w)
+#     ax.set_ylim(h, 0)
+#     ax.imshow(mapa, cmap='binary')
+#     ax.set_title("rrt_elipse_aiw_ldiw", y=1.05, fontsize=14)
+    
+
+#     ax.scatter(start[0], start[1], c='red', s=100, marker='X', zorder=5)
+#     ax.scatter(goal[0], goal[1], c='green', s=100, marker='X', zorder=5)
+
+#     start_node = treeNode(start[0], start[1])
+#     tree = Tree(start_node)
+
+#     sr_x, sr_y, a, b, theta = get_ellipse_params(start, goal)
+#     ellipse_patch = patches.Ellipse((sr_x, sr_y), width=2*a, height=2*b, angle=math.degrees(theta), 
+#                                     fill=False, color='orange', linestyle='--', alpha=0.5, linewidth=1)
+#     ax.add_patch(ellipse_patch)
+
+#     iter_text = ax.text(0, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+#     weight_text = ax.text(0.2, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+#     succes_rate_text = ax.text(0.4, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+
+#     plt.pause(1)
+#     success = 0
+#     ps = 0.0
+#     pa = pmin
+    
+#     for i in range(1,max_iter + 1):
+
+#         ps = success / i
+
+#         if ps < 1e-6:
+#             ps = 1e-6
+        
+#         time_factor = (max_iter-i) / max_iter   
+#         term_1 = (pmax-pmin) / ps
+#         term_2 = (pmax-(pmax-pmin)) / ps
+#         pa = 1 - (term_1 + time_factor*term_2)
+#         pa = np.clip(pa, pmin, pmax)
+
+#         if i % 5 == 0:
+#             iter_text.set_text(f'Iteracja: {i}') 
+#             weight_text.set_text(f'Goal Bias: {pa:.2f}')
+#             succes_rate_text.set_text(f"Succes Rate: {100 * ps:.1f} %")
+
+
+#         if random.random() < pa:
+#             sample_node = treeNode(goal[0], goal[1])
+
+
+#         else:
+#             sample_node = samp_point_elipse(start,goal,mapa)
+
+#         pt, = ax.plot(sample_node.locationX, sample_node.locationY, marker='o', color='red', markersize=15)
+
+#         pot_parent = tree.nearest(sample_node)
+#         distance = tree.distance(pot_parent,sample_node)
+
+#         if distance <= step_len:
+#             new_node = sample_node
+            
+
+#         else:
+#             stepX = ((sample_node.locationX - pot_parent.locationX) / distance) * step_len
+#             stepY = ((sample_node.locationY - pot_parent.locationY) / distance) * step_len
+#             new_node = treeNode(pot_parent.locationX + stepX, pot_parent.locationY + stepY)
+
+#         if collision_free(pot_parent,new_node,mapa):
+#             tree.add_node(new_node,pot_parent)
+#             success += 1
+#             ax.plot([pot_parent.locationX, new_node.locationX], [pot_parent.locationY, new_node.locationY], c='blue', linewidth=2)
+
+
+#             if tree.distance(new_node, treeNode(goal[0], goal[1])) < tolerance:
+#                 tree.add_node(treeNode(goal[0], goal[1]), new_node)
+#                 found_path = tree.path_recovery(tree.nodes[-1])
+#                 for node in found_path:
+#                     if node.parent is None:
+#                         pass
+#                     else:
+#                         ax.plot([node.locationX, node.parent.locationX], [node.locationY, node.parent.locationY],
+#                                 c='green', linewidth=4)
+#                         #plt.pause(0.03)
+
+#                 plt.ioff()
+#                 plt.show()
+
+#                 return f"Path found! \nIterations: {i} \nFinal Goal Bias: {pa:.2f} \nSucces Rate: {100 * ps:.1f} % "
+
+#         #plt.pause(0.001)
+#         try:
+#             pt.remove()
+#         except:
+#             pass
+
+#     return "Path not found"
+
+# def rrt_ellipse_aiw_ldiw_star(start, goal, mapa, step_len = 40, max_iter = 1000, tolerance = 3 , pmax = 0.4 , pmin = 0.05):
+
+#     RRT_STAR_RADIUS = 5 * step_len
+#     plt.ion()
+#     h, w = mapa.shape
+    
+#     fig, ax = plt.subplots(figsize=(12, 7)) 
+
+#     ax.set_xlim(0, w)
+#     ax.set_ylim(h, 0)
+#     ax.imshow(mapa, cmap='binary')
+#     ax.set_title("rrt_elipse_aiw_ldiw", y=1.05, fontsize=14)
+    
+
+#     ax.scatter(start[0], start[1], c='red', s=100, marker='X', zorder=5)
+#     ax.scatter(goal[0], goal[1], c='green', s=100, marker='X', zorder=5)
+
+#     start_node = treeNode(start[0], start[1])
+#     tree = Tree(start_node)
+
+#     sr_x, sr_y, a, b, theta = get_ellipse_params(start, goal)
+#     ellipse_patch = patches.Ellipse((sr_x, sr_y), width=2*a, height=2*b, angle=math.degrees(theta), 
+#                                     fill=False, color='orange', linestyle='--', alpha=0.5, linewidth=1)
+#     ax.add_patch(ellipse_patch)
+
+#     iter_text = ax.text(0, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+#     weight_text = ax.text(0.2, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+#     succes_rate_text = ax.text(0.4, 1.02, '', transform=ax.transAxes, fontsize=8, color='blue', fontweight='bold')
+
+#     plt.pause(1)
+#     success = 0
+#     ps = 0.0
+#     pa = pmin
+    
+#     for i in range(1,max_iter + 1):
+
+#         ps = success / i
+
+#         if ps < 1e-6:
+#             ps = 1e-6
+        
+#         time_factor = (max_iter-i) / max_iter   
+#         term_1 = (pmax-pmin) / ps
+#         term_2 = (pmax-(pmax-pmin)) / ps
+#         pa = 1 - (term_1 + time_factor*term_2)
+#         pa = np.clip(pa, pmin, pmax)
+
+#         if i % 5 == 0:
+#             iter_text.set_text(f'Iteracja: {i}') 
+#             weight_text.set_text(f'Goal Bias: {pa:.2f}')
+#             succes_rate_text.set_text(f"Succes Rate: {100 * ps:.1f} %")
+
+
+#         if random.random() < pa:
+#             sample_node = treeNode(goal[0], goal[1])
+#         else:
+#             sample_node = samp_point_elipse(start,goal,mapa)
+
+#         #pt, = ax.plot(sample_node.locationX, sample_node.locationY, marker='o', color='red', markersize=15)
+
+#         pot_parent = tree.nearest(sample_node)
+#         distance = tree.distance(pot_parent,sample_node)
+
+#         if distance <= step_len:
+#             new_node = sample_node
+            
+
+#         else:
+#             stepX = ((sample_node.locationX - pot_parent.locationX) / distance) * step_len
+#             stepY = ((sample_node.locationY - pot_parent.locationY) / distance) * step_len
+#             new_node = treeNode(pot_parent.locationX + stepX, pot_parent.locationY + stepY)
+
+#         near_nodes = tree.near(new_node, RRT_STAR_RADIUS)
+#         best_parent = pot_parent
+
+#         if pot_parent:
+#             min_cost = pot_parent.cost + tree.distance(pot_parent, new_node)
+#         else:
+#             min_cost = float('inf')
+#         for near_node in near_nodes:
+#             new_path_cost = near_node.cost + tree.distance(near_node, new_node)
+
+#             if new_path_cost < min_cost and collision_free(near_node, new_node, mapa):
+#                 min_cost = new_path_cost
+#                 best_parent = near_node
+
+#         new_node.cost = min_cost
+#         pot_parent = best_parent
+
+#         if pot_parent and collision_free(pot_parent, new_node, mapa):
+#             tree.add_node(new_node, pot_parent)
+#             success += 1
+#             ax.plot([pot_parent.locationX, new_node.locationX], [pot_parent.locationY, new_node.locationY], c='blue', linewidth=1)
+
+#             for near_node in near_nodes:
+#                 rewire_cost = new_node.cost + tree.distance(new_node, near_node)
+                
+#                 if rewire_cost < near_node.cost and collision_free(new_node, near_node, mapa):
+                    
+#                     tree.rewire(near_node, new_node)
+#                     near_node.cost = rewire_cost
+#                     tree.update_descendant_costs(near_node)
+
+
+#             if tree.distance(new_node, treeNode(goal[0], goal[1])) < tolerance:
+#                 tree.add_node(treeNode(goal[0], goal[1]), new_node)
+#                 found_path = tree.path_recovery(tree.nodes[-1])
+#                 for node in found_path:
+#                     if node.parent is None:
+#                         pass
+#                     else:
+#                         ax.plot([node.locationX, node.parent.locationX], [node.locationY, node.parent.locationY],
+#                                 c='green', linewidth=4)
+#                         #plt.pause(0.03)
+
+#                 plt.ioff()
+#                 plt.show()
+
+#                 return f"Path found! \nIterations: {i} \nFinal Goal Bias: {pa:.2f} \nSucces Rate: {100 * ps:.1f} % "
+
+#         #plt.pause(0.001)
+#         try:
+#             pt.remove()
+#         except:
+#             pass
+
+#     return "Path not found"
